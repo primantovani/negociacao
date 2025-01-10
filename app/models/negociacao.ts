075037980330711f0b11a813cf13180cc5b0fea1
primantovani <APRISCILAMANTOVANI@GMAIL.COM>
2025-01-10 15:16:10 -0800

    Adicionando metodos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,27 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    private _data;
+    private _quantidade;
+    private _valor;
 
-    constructor(data: Date, quantidade: number, valor: number) {
+    constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
-    get data(): Date {
+    get data() {
         return this._data;
     }
 
-    get quantidade():number {
+    get quantidade() {
         return this._quantidade;
     }
 
-    get valor(): number {
+    get valor() {
         return this._valor;
     }
 
-    get volume(): number {
+    get volume() {
         return this._quantidade * this._valor;
     }
 }
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,19 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
+
+
+}
+
+const negociacoes = new Negociacoes();
+negociacoes.lista().forEach(n => {
+
+})
